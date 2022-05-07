9425601cb5afea4aae9d981d135cc1d1443168cc
Dmitry <p.dmitry.y@gmail.com>
2022-05-08 02:57:43 +0300

    feat: make layout of keyboard

--- Virtual-Keyboard/src/index.js
+++ Virtual-Keyboard/src/index.js
@@ -2,8 +2,13 @@ import keyLang from './keys.js';
 
 const keysEn = keyLang['en']['default'];
 const keysEnShift = keyLang['en']['shift'];
+const keysRu = keyLang['ru']['default'];
+const keysRuShift = keyLang['ru']['shift'];
 
-console.log(keysEnShift);
+let currentLang = keysEn;
+let currentLangShift = keysEnShift;
+
+console.log(currentLang);
 
 const wrapper = document.createElement('div');
 wrapper.classList.add('wrapper');
@@ -23,26 +28,87 @@ wrapper.appendChild(text_area);
 wrapper.appendChild(keyboard);
 keyboard.appendChild(keys);
 
+function createIconHTML(icon_name) {
+  return `<i class="material-icons">${icon_name}</i>`;
+}
+
 function addKeys() {
-  for (let key in keysEn) {
+  //let i = 0;
+  currentLang.forEach((key, index) => {
     const keyElement = document.createElement('button');
     const upperKeyElement = document.createElement('div');
     upperKeyElement.classList.add('upperKey');
     keyElement.classList.add('key');
     keyElement.textContent = key;
-    upperKeyElement.textContent = keysEnShift[key];
+    upperKeyElement.textContent = currentLangShift[index];
     switch (key){
       case 'Backspace':
         keyElement.classList.add('middle-width');
+        break;
       case 'Caps Lock':
         keyElement.classList.add('middle-width');
-      case '`':
-        keyElement.setAttribute('order', 1)
+        keyElement.classList.add('caps');
+        keyElement.addEventListener('click', () => {
+          keyElement.classList.toggle('active');
+        });
+        break;
+      case 'ENTER':
+        keyElement.classList.add('middle-width');
+        break;
+      case 'Shift':
+        keyElement.classList.add('middle-width');
+        break;
+      case 'Space':
+        keyElement.innerText = '';
+        keyElement.classList.add('lage-width');
+        break;
+      case 'Ctrl':
+        keyElement.classList.add('bigger-width');
+      break;
     }
     keyboard.appendChild(keyElement);
+    if (key == 'KeyUp') {
+      keyElement.innerHTML = createIconHTML('keyboard_arrow_up')
+    }
+    if (key == 'KeyLeft') {
+      keyElement.innerHTML = createIconHTML('keyboard_arrow_left')
+    }
+    if (key == 'KeyDown') {
+      keyElement.innerHTML = createIconHTML('keyboard_arrow_down')
+    }
+    if (key == 'KeyRight') {
+      keyElement.innerHTML = createIconHTML('keyboard_arrow_right')
+    }
     keyElement.appendChild(upperKeyElement);
+    keyElement.addEventListener('mousedown', () => {
+      keyElement.classList.add('pressed');
+    });
+    keyElement.addEventListener('mouseup', () => {
+      keyElement.classList.remove('pressed');
+    });
+  });
+  // for (let key in currentLang) {
+  //   const keyElement = document.createElement('button');
+  //   const upperKeyElement = document.createElement('div');
+  //   upperKeyElement.classList.add('upperKey');
+  //   keyElement.classList.add('key');
+  //   keyElement.textContent = key;
+  //   upperKeyElement.textContent = currentLangShift[key];
+  //   console.log(key)
+  //   switch (key){
+  //     case 'Backspace':
+  //       keyElement.classList.add('middle-width');
+  //     case 'Caps Lock':
+  //       keyElement.classList.add('middle-width');
+  //     case 'ENTER':
+  //       keyElement.classList.add('middle-width');
+  //     case 'Shift':
+  //       keyElement.classList.add('middle-width');
+        
+  //   }
+
   }
-}
+
 
 window.addEventListener("DOMContentLoaded", function () {
   addKeys();
